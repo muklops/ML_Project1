d337b9b67f6c4a978b0427ccc0ad66ca0a0a0a22
Mukesh Kumar <muklops@gmail.com>
2025-12-09 21:20:49 +0530

    ok

--- src/MLProject/config/configuration.py
+++ src/MLProject/config/configuration.py
@@ -1,6 +1,6 @@
 from src.MLProject.constants import *
 from src.MLProject.utils.common import read_yaml, create_directories
-from src.MLProject.entity.config_entity import (DataIngestionConfig,DataValidationConfig)
+from src.MLProject.entity.config_entity import (DataIngestionConfig,DataValidationConfig,DataTransformationConfig)
 
 
 
@@ -48,3 +48,16 @@ class ConfigurationManager:
             all_schema=schema
         )
         return data_validation_config
+    
+    
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+        
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=Path(config.root_dir),
+            data_path=Path(config.data_path
+        ))
+        return data_transformation_config
